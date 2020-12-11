6c8b6c43a1022f92bc37862026bb0c249c4415f9
Klumpke <jeroen@klompen.biz>
2020-12-11 22:49:13 +0100

    test

--- s0pcm/run.sh
+++ s0pcm/run.sh
@@ -3,17 +3,18 @@
 CONFIG_PATH=/data/options.json
 
 DATA_PATH=$(jq --raw-output '.data_path' $CONFIG_PATH)
+
+# Check if config exists already
 # Copy example configuration if not exists
+mkdir -p $DATA_PATH
 if [ ! -f $DATA_PATH/configuration.yaml ]; then
   cp ./configuration.yaml.example $DATA_PATH/configuration.yaml
 fi
-
 
 # Start s0pcmreader
 echo "[INFO] Start S0PCM Reader"
 python ./s0pcm-reader.py -c $DATA_PATH
 S0PCMREADER_PID=$!
-
 
 if [ -f $DATA_PATH/.noexit ]; then
   sleep 7d
