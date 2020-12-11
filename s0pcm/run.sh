5794fe5e7df4048169c715fc7b8f1a9bdf24250f
Klumpke <jeroen@klompen.biz>
2020-12-11 22:33:27 +0100

    Update run.sh

--- s0pcm/run.sh
+++ s0pcm/run.sh
@@ -8,7 +8,12 @@ if [ ! -f $DATA_PATH/configuration.yaml ]; then
   cp ./configuration.yaml.example $DATA_PATH/configuration.yaml
 fi
 
+
+# Start s0pcmreader
+echo "[INFO] Start S0PCM Reader"
 python ./s0pcm-reader.py -c $DATA_PATH
+S0PCMREADER_PID=$!
+
 
 if [ -f $DATA_PATH/.noexit ]; then
   sleep 7d
